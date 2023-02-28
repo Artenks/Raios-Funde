897164164239f31c1ebd71fccb19efb929fed712
artenks <artenkes@gmail.com>
2023-02-28 20:53:46 -0300

    adicionando o score

---
+++ Assets/Scripts/Game/GameScoreView.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class GameScoreView : MonoBehaviour
+{
+    public TMP_Text ScoreCombo;
+    public TMP_Text ScoreRecord;
+
+    private GameScore _gameScore;
+    void Awake()
+    {
+        _gameScore = GetComponent<GameScore>();
+        _gameScore.UpdateScoreEventHandler += _gameScore_UpdateScoreEventHandler;
+
+        _gameScore_UpdateScoreEventHandler(_gameScore.Score.Combo, _gameScore.Score.Record);
+    }
+
+    private void _gameScore_UpdateScoreEventHandler(int combo, int record)
+    {
+        if (combo > 0)
+        {
+            ScoreCombo.gameObject.SetActive(true);
+            ScoreCombo.text = $"Combo: {combo}";
+        }
+        else
+            ScoreCombo.gameObject.SetActive(false);
+
+        if (record > 0)
+        {
+            ScoreRecord.gameObject.SetActive(true);
+            ScoreRecord.text = $"Record: {record}";
+        }
+        else
+            ScoreRecord.gameObject.SetActive(false);
+    }
+
+}
