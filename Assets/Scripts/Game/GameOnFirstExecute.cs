16202cb1a25221c0e168bcced9eeb0a45bcb4151
artenks <artenkes@gmail.com>
2023-02-28 02:06:24 -0300

    organizando scripts

---
+++ Assets/Scripts/Game/GameOnFirstExecute.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+public class GameOnFirstExecute : MonoBehaviour
+{
+    public event Action FirstTimeEventHandler;
+
+    public event Action MusicResetStateEventHandler;
+
+    public ConfigSave ConfigSave;
+    public ResolutionScript ResolutionScript;
+    public MusicManager MusicManager;
+
+    public void FirstExecuteInvoke()
+    {
+        if (ConfigSave.Info.noIsFirstTime == false)
+        {
+            MusicResetStateEventHandler?.Invoke();
+            ResolutionScript.ResolutionOnFirstExecute();
+            FirstTimeEventHandler?.Invoke();
+        }
+        else
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameModes/GameModesInfo.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using UnityEngine;
+
+public class GameModesInfo : MonoBehaviour
+{
+    public struct GameModeInfo
+    {
+        public float Timer;
+        public int TimerID;
+        public int Tips;
+        public int Chances;
+
+    }
+    public GameModeInfo Info;
+    public GameManager Manager;
+    private string _path;
+
+    private void Awake()
+    {
+        _path = Application.persistentDataPath + "/GameModes.json";
+
+        if (File.Exists(_path))
+        {
+            var file = File.OpenText(_path);
+            file.Close();
+        }
+        else
+        {
+            var file = File.CreateText(_path);
+            file.Close();
+        }
+        Load();
+    }
+
+    private void Save()
+    {
+        var contentList = JsonUtility.ToJson(Info, true);
+
+        File.WriteAllText(_path, contentList);
+
+        Manager.ConfigurateGameModes();
+    }
+
+    private void Load()
+    {
+        var contentPath = File.ReadAllText(_path);
+
+        if (contentPath == "")
+        {
+            Save();
+            return;
+        }
+
+        var content = JsonUtility.FromJson<GameModeInfo>(contentPath);
+
+        Info.Timer = content.Timer;
+        Info.TimerID = content.TimerID;
+        Info.Chances = content.Chances;
+        Info.Tips = content.Tips;
+    }
+
+    public void GameStats(string type, float ItemNumber)
+    {
+        switch (type)
+        {
+            case "timer":
+                Info.TimerID = (int)ItemNumber;
+                break;
+            case "tips":
+                Info.Tips = (int)ItemNumber;
+                break;
+            case "chances":
+                Info.Chances = (int)ItemNumber;
+                break;
+        }
+        Save();
+    }
+}
