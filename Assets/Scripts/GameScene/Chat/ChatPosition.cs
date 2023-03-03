424d58eda6f85f548cd565431c6bbb6cf1cb0db2
artenks <artenkes@gmail.com>
2023-03-03 20:19:27 -0300

    atualizando melhor posicao para chat em um dos menu

--- Assets/Scripts/GameScene/Chat/ChatPosition.cs
+++ Assets/Scripts/GameScene/Chat/ChatPosition.cs
@@ -21,32 +21,26 @@ public class ChatPosition : MonoBehaviour
         if (scene.name == "MainMenu")
         {
             thisChat.localPosition = new Vector3(740, -259, 45.5968933f);
-            Debug.Log("menu");
         }
         if (scene.name == "ChoiceModeMenu")
         {
-            thisChat.localPosition = new Vector3(-694.616699f, -277, 45.5968933f);
-            Debug.Log("choice");
+            thisChat.localPosition = new Vector3(731, -267, 45.5968933f);
         }
         if (scene.name == "TwitchMenu")
         {
             thisChat.localPosition = new Vector3(-744, -252, 45.5968933f);
-            Debug.Log("twitch");
         }
         if (scene.name == "SimpleGame")
         {
             thisChat.localPosition = new Vector3(0, -255, 45.5968933f);
-            Debug.Log("game");
         }
         if (scene.name == "CreateMode")
         {
             thisChat.localPosition = new Vector3(0, -255, 45.5968933f);
-            Debug.Log("create");
         }
         if (scene.name == "FinishGame")
         {
             thisChat.localPosition = new Vector3(0, -255, 45.5968933f);
-            Debug.Log("finish");
         }
     }
 }
