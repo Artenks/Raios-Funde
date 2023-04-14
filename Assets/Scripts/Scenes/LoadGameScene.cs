a6f1f2ab697833987981b2748f01e936261042f4
artenks <artenkes@gmail.com>
2023-04-14 17:56:31 -0300

    raios funde terminacoes

--- Assets/Scripts/Scenes/LoadGameScene.cs
+++ Assets/Scripts/Scenes/LoadGameScene.cs
@@ -4,10 +4,8 @@ using UnityEngine.SceneManagement;
 public class LoadGameScene : MonoBehaviour
 {
     public int sceneToLoadIndex;
-    public int sceneToUnloadIndex;
     private void OnEnable()
     {
-        SceneManager.LoadSceneAsync(sceneToLoadIndex);
-        SceneManager.UnloadSceneAsync(sceneToUnloadIndex);
+        SceneManager.LoadScene(sceneToLoadIndex);
     }
 }
